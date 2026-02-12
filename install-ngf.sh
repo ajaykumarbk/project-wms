7a65b773d294b3ceead8ee6ba09d8a38bb4d1ee5
ajaykumarbk <ajaykumarbk34@gmail.com>
2026-02-12 10:05:45 +0530

    added cluster setup scripts

---
+++ install-ngf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Installing helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
+
+#Standard  CRD
+kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.4.1" | kubectl apply -f -
+
+echo " Installin gateway api crd "
+# Experimental CRDs
+kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/experimental?ref=v2.4.1" | kubectl apply -f -
+
+echo " Installingnginx gate fabric using helm-"
+helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric \
+  --create-namespace \
+  -n nginx-gateway
+
+echo "Waiting for deployment ready"
+kubectl wait --timeout=5m -n nginx-gateway deployment/ngf-nginx-gateway-fabric --for=condition=Available
+
+# Install Cert-Manager
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
+
+echo "Installation complete"
